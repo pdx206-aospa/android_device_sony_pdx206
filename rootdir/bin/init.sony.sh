fc0ed4fd2fcc00c447f42ee959bb9b4c6a2f3a84
ahnet-69 <syedmusicrealise@gmail.com>
2024-02-02 15:56:54 +0500

    pdx206: rootdir: Remove multiple setting of same prop

--- rootdir/bin/init.sony.sh
+++ rootdir/bin/init.sony.sh
@@ -2,12 +2,6 @@
 
 model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
-# NFC
-case "$model" in
-    "XQ-AS42" )
-        setprop persist.vendor.nfc.config_file_name libnfc-nxp-typef.conf;;
-esac
-
 # Radio
 case "$model" in
     "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
