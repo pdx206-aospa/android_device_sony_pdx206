6fa387ed3d8b62895f66d47350c98bfa6d607d1d
ahnet-69 <syedmusicrealise@gmail.com>
2024-02-04 20:31:24 +0500

    pdx206: rootdir: Fix correct type-f nfc config not being set for japense carrier models
    In the previous implementation the model was being reported as "", Which was causing it to be set as unknown. Now the new implementation checks if there is mention of the specific japense model in the LTALABEL path and if it is, the model is set accordingly. Otherwise the following error code would happen: libnfc_nci: unknown opcode:0x21'
    
    This implementation might be janky but works and was written with my limited knowledge of sh files.
    
    Test: Take a logcat and "libnfc_nci: unknown opcode:0x21'" error does not occur, Nfc works as expected
    Tested by: Nezhdan (on xda)

--- rootdir/bin/init.sony.sh
+++ rootdir/bin/init.sony.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
-
-model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
+ltapath="/dev/block/bootdevice/by-name/LTALabel"
+model=`grep -aim1 'model:' "$ltapath" | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
 # Radio
 case "$model" in
@@ -11,7 +11,26 @@ case "$model" in
 esac
 
 if [ "$model" == "" ]; then
-    setprop vendor.radio.ltalabel.model "unknown"
+
+carrier="false"
+
+    # Check if they are japense carrier models
+    if grep -aim1 "SOG02" "$ltapath" > /dev/null; then
+        carrier="true"
+        setprop vendor.radio.ltalabel.model "SOG02"
+    fi
+    if grep -aim1 "A002SO" "$ltapath" > /dev/null; then
+        carrier="true"
+        setprop vendor.radio.ltalabel.model "A002SO"
+    fi
+    if grep -aim1 "SO-52A" "$ltapath" > /dev/null; then
+        carrier="true"
+        setprop vendor.radio.ltalabel.model "SO-52A"
+    fi
+    if [ ! "$carrier" == "true" ]; then
+        setprop vendor.radio.ltalabel.model "unknown"
+    fi
+
 else
     setprop vendor.radio.ltalabel.model "$model"
 fi
