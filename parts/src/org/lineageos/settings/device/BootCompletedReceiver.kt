64ff32e0cc6bf6a76e7fbd90262a06db5d712041
ahnet-69 <syedmusicrealise@gmail.com>
2024-02-04 13:39:25 +0500

    pdx206: parts: Add Dolby Sound tile service

--- parts/src/org/lineageos/settings/device/BootCompletedReceiver.kt
+++ parts/src/org/lineageos/settings/device/BootCompletedReceiver.kt
@@ -9,12 +9,25 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.preference.PreferenceManager
 import org.lineageos.settings.device.display.CreatorModeUtils
+import org.lineageos.settings.device.dolby.tile.DolbyCore;
+import org.lineageos.settings.device.dolby.tile.DolbyFragment.Companion.PREF_DOLBY_ENABLE
+import org.lineageos.settings.device.dolby.tile.DolbyFragment.Companion.PREF_DOLBY_MODES
 
 class BootCompletedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         Log.d(TAG, "Starting")
         CreatorModeUtils(context).initialize()
+
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        for ((key, value) in PREF_DOLBY_MODES) {
+            if (sharedPrefs.getBoolean(key, false)) {
+                DolbyCore.setProfile(value)
+                break
+            }
+        }
+        DolbyCore.setEnabled(sharedPrefs.getBoolean(PREF_DOLBY_ENABLE, false))
     }
 
     companion object {
