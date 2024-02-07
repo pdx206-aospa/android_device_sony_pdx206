5b5cf310bcfef272c10356738c45001b7af27cc7
ahnet-69 <syedmusicrealise@gmail.com>
2024-02-07 19:22:10 +0500

    Revert "pdx206: Bring back DT2W"
    This reverts commit a15abe6ba4d56d7004c1ba18f5d1df4268b63606.

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -90,12 +90,11 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     }
 #endif
     switch (type) {
+#ifdef TAP_TO_WAKE_NODE
         case Mode::DOUBLE_TAP_TO_WAKE:
-            ::android::base::WriteStringToFile(enabled ? "sod_enable,1" : "sod_enable,0",
-                                               "/sys/devices/virtual/sec/tsp/cmd");
-            ::android::base::WriteStringToFile(enabled ? "1" : "0",
-                                               "/sys/devices/dsi_panel_driver/pre_sod_mode");
+            ::android::base::WriteStringToFile(enabled ? "1" : "0", TAP_TO_WAKE_NODE, true);
             break;
+#endif
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled) {
                 HintManager::GetInstance()->DoHint("SUSTAINED_PERFORMANCE");
@@ -107,6 +106,10 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
                 break;
             }
             [[fallthrough]];
+#ifndef TAP_TO_WAKE_NODE
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            [[fallthrough]];
+#endif
         case Mode::FIXED_PERFORMANCE:
             [[fallthrough]];
         case Mode::EXPENSIVE_RENDERING:
@@ -139,8 +142,13 @@ ndk::ScopedAStatus Power::isModeSupported(Mode type, bool *_aidl_return) {
 #endif
 
     bool supported = HintManager::GetInstance()->IsHintSupported(toString(type));
-    // LOW_POWER and DOUBLE_TAP_TO_WAKE handled insides PowerHAL specifically
-    if (type == Mode::LOW_POWER || type == Mode::DOUBLE_TAP_TO_WAKE) {
+#ifdef TAP_TO_WAKE_NODE
+    if (type == Mode::DOUBLE_TAP_TO_WAKE) {
+        supported = true;
+    }
+#endif
+    // LOW_POWER handled insides PowerHAL specifically
+    if (type == Mode::LOW_POWER) {
         supported = true;
     }
     LOG(INFO) << "Power mode " << toString(type) << " isModeSupported: " << supported;
