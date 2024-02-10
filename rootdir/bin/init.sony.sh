4f2608b8660cfe0f071f6afc6b23e2d93a6afd3a
ahnet-69 <syedmusicrealise@gmail.com>
2024-02-10 19:52:59 +0500

    pd206: init: Fix NFC for global japense models again

--- rootdir/bin/init.sony.sh
+++ rootdir/bin/init.sony.sh
@@ -12,22 +12,30 @@ esac
 
 if [ "$model" == "" ]; then
 
-carrier="false"
+typef="false"
 
-    # Check if they are japense carrier models
+    # Check if they are japense felica models
+    if grep -aim1 "XQ-AS42" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop persist.vendor.nfc.config_file_name "libnfc-nxp-typef.conf"
+    fi
+    if grep -aim1 "XQ-AT42" "$ltapath" > /dev/null; then
+        typef="true"
+        setprop persist.vendor.nfc.config_file_name "libnfc-nxp-typef.conf"
+    fi
     if grep -aim1 "SOG02" "$ltapath" > /dev/null; then
-        carrier="true"
+        typef="true"
         setprop persist.vendor.nfc.config_file_name "libnfc-nxp-typef.conf"
     fi
     if grep -aim1 "A002SO" "$ltapath" > /dev/null; then
-        carrier="true"
+        typef="true"
         setprop persist.vendor.nfc.config_file_name "libnfc-nxp-typef.conf"
     fi
     if grep -aim1 "SO-52A" "$ltapath" > /dev/null; then
-        carrier="true"
+        typef="true"
         setprop persist.vendor.nfc.config_file_name "libnfc-nxp-typef.conf"
     fi
-    if [ ! "$carrier" == "true" ]; then
+    if [ ! "$typef" == "true" ]; then
         setprop persist.vendor.nfc.config_file_name "libnfc-nxp.conf"
     fi
 
