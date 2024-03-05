ffd2fa5ee7c10a4a0e30c685e43a0f382f66d658
ahnet-69 <syedmusicrealise@gmail.com>
2024-03-05 12:16:47 +0000

    pdx206: vendorsetup: Shallow clone for all of the repos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,14 +4,14 @@ echo 'Start Cloning repos for pdx206'
 echo 'Cloning Vendor tree [1/3]'
 # Proprietary blobs
 rm -rf vendor/sony/pdx206
-git clone https://github.com/ProjectElixir-Devices/vendor_sony_pdx206.git -b fourteen vendor/sony/pdx206
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_sony_pdx206.git -b fourteen vendor/sony/pdx206
 
 echo 'Cloning Vendor extras [2/3]'
 # Dolby/Sony Camera Pro apps
 rm -rf vendor/sony/pdx206-extra
-git clone https://github.com/ProjectElixir-Devices/vendor_sony_pdx206-extra.git -b fourteen vendor/sony/pdx206-extra
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_sony_pdx206-extra.git -b fourteen vendor/sony/pdx206-extra
 
 echo 'Cloning Kernel Source [3/3]'
 # Kernel source
 rm -rf kernel/sony/sm8250
-git clone https://github.com/ProjectElixir-Devices/kernel_sony_sm8250.git -b fourteen kernel/sony/sm8250
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_sony_sm8250.git -b fourteen kernel/sony/sm8250
