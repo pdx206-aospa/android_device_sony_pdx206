9132c3c2342fe9e2ba38ce653b26f028fb27c8d8
ahnet-69 <syedmusicrealise@gmail.com>
2024-04-03 20:16:08 +0000

    pdx206: device.mk: Dont include BCR
    I mean it doesnt exist so?

--- device.mk
+++ device.mk
@@ -601,8 +601,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XperiaParts
 
-#Basic Call Recorder
-$(call inherit-product, vendor/bcr/bcr.mk)
-
 # Inherit from vendor blobs
 $(call inherit-product, vendor/sony/pdx206/pdx206-vendor.mk)
